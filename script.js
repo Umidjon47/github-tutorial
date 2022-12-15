6a96e6a2adaa32ab75a7ff8cd670fae8dc87b667
Umidjon <umidjontillavoldiyev5@gmail.com>
2022-12-15 21:18:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const color = [];
+color[0] = prompt("What's your favourite color #1", "");
+color[1] = prompt("What's your favourite color #2", "");
+color[2] = prompt("What's your favourite color #3", "");
+
+console.log(color);
+
+// const color = prompt("dbajdjhasbjdh");
+
+// switch(color){
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "yellow":
+//         console.log("Slowly");
+//         break;
+//     case "green":
+//         console.log("Goo");
+//         break;
+//     default:
+//         console.log("Traffik jam");
+// };
+
